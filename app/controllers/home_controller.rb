e2ade50044dcebb4a2091056e4d6ea811c7f3c96
Mateo Blanc <mateoblanc93@gmail.com>
2020-10-21 11:38:26 +0200

    force ssl use

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,11 +11,18 @@ class HomeController < ApplicationController
         # re-initialize Home object for cleared form
         @contact = Home.new
         format.html { render 'index'}
-        format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+        format.js   { flash.now[:success] = "Thank you for your message. I'll get back to you soon!" }
+        puts(params[:name])
       else
         format.html { render 'index' }
         format.js   { flash.now[:error] = @message = "Message did not send." }
       end
     end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :phone_number, :body)
   end
 end
